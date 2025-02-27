23a5ea4d08b32d7648f373600be38145aa7fa6d7
innerNULL <lyt697@outlook.com>
2025-02-27 11:36:25 +0800

    chore: Update default LLM

--- bin/llm_serv_vllm.sh
+++ bin/llm_serv_vllm.sh
@@ -8,14 +8,15 @@ CURR_DIR=$(pwd)
 WORKSPACE="./_llm_serv_vllm"
 PYTHON=$(which python3)
 PORT=6789
+VLLM_VERSION="0.7.3"
 HF_TOKEN=""
-MODEL="google/gemma-2-2b-it"
-CUDA_VISIBLE_DEVICES=3,4
-MAX_MODEL_LEN=5000
-TENSOR_PARALLEL_SIZE=2
+MODEL="mistralai/Mistral-7B-Instruct-v0.3"
+CUDA_VISIBLE_DEVICES=3
+MAX_MODEL_LEN=20000
+TENSOR_PARALLEL_SIZE=1
 GPU_MEM_UTILIZATION=0.5
 DTYPE=bfloat16
-VLLM_ALLOW_LONG_MAX_MODEL_LEN=0
+VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
 
 
 
@@ -29,7 +30,7 @@ function build() {
   cd ${CURR_DIR}
   cd ${WORKSPACE}
   ${PYTHON} -m venv ./_pyenv --copies
-  ./_pyenv/bin/pip install vllm
+  ./_pyenv/bin/pip install vllm==${VLLM_VERSION}
 }
 
 
@@ -45,13 +46,13 @@ function start() {
     --tensor-parallel-size ${TENSOR_PARALLEL_SIZE} \
     --gpu-memory-utilization ${GPU_MEM_UTILIZATION} \
     --port ${PORT} \
-    #--enable-prefix-caching 
+    --enable-prefix-caching 
 }
 
 
 function main() {
-  #init
-  #build
+  init
+  build
   start
 }
 
