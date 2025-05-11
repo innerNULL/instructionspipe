fab606340efcb2f26faba736c6d4fe4cd2fc3e1a
innerNULL <lyt697@outlook.com>
2025-05-11 23:19:23 +0800

    feat: Init torch based distributed trainer

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-export NIX_CONF_DIR="$(pwd)" 
-nix-shell --pure shell.nix
+set -x
+
+export NIX_CONF_DIR="$(pwd)"
+export NIX_PATH="$(pwd)/configuration.nix${NIX_PATH:+:}$NIX_PATH"
+NIXPKGS_ALLOW_UNFREE=1 nix-shell \
+  --impure shell.nix \
+  --extra-nix-path nixos-config=$(pwd)/configuration.nix
